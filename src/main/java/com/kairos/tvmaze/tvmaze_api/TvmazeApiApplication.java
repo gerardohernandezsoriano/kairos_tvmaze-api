19f8aaf8b2d4ab9eddb1406365c78277760bd5c6
gerardohernandezsorianodev <gerardohernandezsorianodev@gmail.com>
2026-09-22 14:54:49 -0600

    chore: initialize Spring Boot project add hexagonal architecture structure

---
+++ src/main/java/com/kairos/tvmaze/tvmaze_api/TvmazeApiApplication.java
@@ -0,0 +1,13 @@
+package com.kairos.tvmaze.tvmaze_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmazeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvmazeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kairos/tvmaze/tvmaze_api/TvmazeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairos.tvmaze.tvmaze_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmazeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
